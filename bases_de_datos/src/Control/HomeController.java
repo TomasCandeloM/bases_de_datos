924d82a50247869790367ddf278293ba2f18624d
TomasCandeloM <tomascamo@unisabana.edu.co>
2022-10-20 15:37:28 -0500

    Commit test 2

--- bases_de_datos/src/Control/HomeController.java
+++ bases_de_datos/src/Control/HomeController.java
@@ -21,7 +21,7 @@ public class HomeController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        int var = 4;
+        int hola = 4;
     }    
     
 }
